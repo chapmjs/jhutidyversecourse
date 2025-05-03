c71dd8cba8fece5b288bb953998e779c769cd5ab
John Chapman <john.chapman@systemsresearch.linkpc.net>
2025-05-02 18:44:45 -0600

    rename files

---
+++ jhu_tidyverse_course_ch_01.R
@@ -0,0 +1,17 @@
+# Install packages
+# install.packages("tidyverse")
+# install.packages("here")
+# install.packages("ProjectTemplate")
+
+
+library(tidyverse)
+library(here)
+library(ProjectTemplate)
+
+# 1.6.3.1
+getwd()
+here()
+
+# 1.6.5
+create.project(project.name = "health_expenditures",
+               template = "minimal")
---
+++ jhu_tidyverse_course_ch_02.R
@@ -0,0 +1,3 @@
+# jhu_tidy_verse_course_ch_02
+# 20250502
+

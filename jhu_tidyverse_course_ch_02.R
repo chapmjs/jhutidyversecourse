532c82de910bd5ce660b7ed38c98b3da20da7394
John Chapman <john.chapman@systemsresearch.linkpc.net>
2025-05-03 10:28:08 -0600

    2.3.1.1 Reading excel files into R

--- jhu_tidyverse_course_ch_02.R
+++ jhu_tidyverse_course_ch_02.R
@@ -41,7 +41,7 @@ df$z
 df[[4]]
 
 
-# 2.3.1.1 Readnig Excel files into R
+# 2.3.1.1 Reading Excel files into R
 
 #install.packages("readxl")
 library(readxl)
@@ -50,3 +50,42 @@ library(readxl)
 example <- readxl_example("datasets.xlsx")
 df <- read_excel(example)
 df
+
+# I have a tibble of 32 x 11 (not the 150 x 5 in the book:
+# https://jhudatascience.org/tidyversecourse/get-data.html#reading-excel-files-into-r
+
+# read_excel(example, col_names = LETTERS[1:5])
+read_excel(example, col_names = LETTERS[1:11])
+
+# read example file into R using .name_repair default
+read_excel(
+  readxl_example("deaths.xlsx"),
+  range = "arts!A5:F8",
+  .name_repair = "unique"
+)
+
+
+# require use of universal naming conventions
+read_excel(
+  readxl_example("deaths.xlsx"),
+  range = "arts!A5:F8",
+  .name_repair = "universal"
+)
+
+# pass function for column naming
+read_excel(
+  readxl_example("deaths.xlsx"),
+  range = "arts!A5:F8",
+  .name_repair = toupper
+)
+
+# pass function for column naming
+deaths <-  read_excel(
+    readxl_example("deaths.xlsx"),
+    range = "arts!A5:F11",
+    .name_repair = "universal"
+  )
+
+
+
+
